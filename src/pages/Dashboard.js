c29f494dcaaf74032ddc9a8c20711bb477822421
paleta77 <pawel050673@gmail.com>
2020-11-25 00:29:48 +0100

    Added logout button

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,14 +1,23 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import {useAuth} from "../use-auth.js";
 
 export function Dashboard(){
     const auth = useAuth();
+
+    let history = useHistory();
+
+    let logout = () => {
+        auth.signout(() => {
+            history.push("/");
+        });
+    };
+
     return (
         <div>
             Witaj na swoim panelu
             <Link to="/">
-                <button>
+                <button onClick={logout}>
                     Wyloguj
                 </button>
             </Link>
