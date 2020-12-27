3a6c2226096bf23531ec83fe575820c2f956ed2c
paleta77 <pawel050673@gmail.com>
2020-12-27 23:20:06 +0100

    added base for removing aliases from server dashboard

--- src/Crypter.js
+++ src/Crypter.js
@@ -18,55 +18,10 @@ function jwtDecode(jwt){
 }
 
 export function decryptString(message, privateKey){
-    // let privateKeyBuilder = "-----BEGIN PRIVATE KEY-----\n";
-    // privateKeyBuilder += "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBALUgiFGogiNrPMqcdL1akwR4SPxX4CN+HlVGfywxRYD55sSRqDdTCN5kR0Rj9bUtegopAgkrnLQmgBpAzyD3ndFLaH8gtOoDiT6cPIuOyldRWyoVpaJ+632KTWanEeyW+j4Vh4+SShwSEORB9rQuMKUTx0qHBROK8+NiPSnUhkWPAgMBAAECgYAWmhKorfBfXxhc6B0hK9ZArondA1Iy26AJYByqdROJPqXvTYXCMZhft/+0pwyYKS6bafaZElohmNRPclrmcD+/Eg7e757qMZ9oqW+2wbd8Qy7CF8HufULvEYsg44JBKomnra2wBcDC4mAKvq3zuS6MrDaKmEEkvLc9I/eI8AADcQJBAPSB//eb5rLqFAc/ecU17e03bnV0nLE7XNpD8cWPPTqhGd/FNwL8gj1lDLLC0vvLaA0RmPO8gmLD+zcc2UmsHosCQQC9o+oT8PNFTWSHlDJcYrPPWnlfFFdHyilwC+J8XUmZfVJPQpMNXnwelMSBGCHVmnV5Th3MQ5cL47f8Znf/1bmNAkAFW4PcTRI1AWQG9KEabSAVfo1xDWkRiRdzwOFfTtAspLMIcC3QZvADoF8y24n78m0JPEV5sbu5pkmu33bzYk4ZAkBqbNHRrRgpHVy8hZuZoz67sE+K31R4Emkc216YYpYKUH1fw8HY79yCIQUCWbOrN0qT8BXi60+aOYqoWC0h4ySFAkEA2y1j5I/zLj/CBcD7HrP7dU8+cTfnq7RdAwZB5hLZjvqy4toeDir6NU5Twny93hyzftdKjlTh8S5QCk2u5FEZvQ==";
-    // privateKeyBuilder += "\n-----END PRIVATE KEY-----";
-    // console.log(privateKeyBuilder);
-    // const userPrivateKey = new NodeRSA("-----BEGIN PRIVATE KEY-----" +
-    //     "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBALhvzMkqrPcHi9nY" +
-    //     "lIOnKdKhy0We1mUQXC4bkjrhej9/BGvI8r4wrtWROw3oO1a54GnWdlqQxzQNyXOY" +
-    //     "gMpJlH0Ew1R7Zk11sgNtEgek0DOZyggdz+lVsVsM5QYe/2YCxZ/LoCNIeCjA+k3W" +
-    //     "GD0/HQnPYOHcMOOkcfE/g3JZokzdAgMBAAECgYAk9lLmJeVQBHUkjWEGGWQqUD56" +
-    //     "j2CMFIFExEXEAxgKzZvHNhhfwUXoixh4qwfpU4sD6vUBJW6mofSzc+aro59cLzOY" +
-    //     "3C0+boErNLJdPZnx7qBEmXLq6t+j46+xwAm9IqvHTwbfKWxDO/zaaJk1LngMR2db" +
-    //     "vmt5NIA+8xcgG+eeCQJBAO6v2bIv0cpN0LPDEb+8r54gMiCSmpeoZZg2VOp4xwte" +
-    //     "Sez+6dgCawSYhhOJHQLzBAfEamb3U0AU/1ygMhvmoqcCQQDF0JV+XSRkVWPsOnTw" +
-    //     "v3pN382dChO5+Excj2ZnBL8Ui4MI9ioJDL9oQWC9OdX6pisO1v8VV6qC0b9+lWEB" +
-    //     "bpjbAkEAv9QmsvWJtvyJMiAhoKaFwiEACHuqsR5O4GEyJ+t/lRxnb4iD6e4vklb+" +
-    //     "0q6M2XT0FUX/eddmWgdGIGxDOXdH2wJAZXKFWFrYMQSBzEJmepkYVhuyzRi/6R3n" +
-    //     "u4hnUenjPNwXhV8JS77X4tW0kRa6IGNbra6g/wcIkTKUbIybRSq9BQJARlm/agkB" +
-    //     "NNEUnXODGzI27fj4c8hDDmsveUsFN9D1BVgjv7lsCze2nSxaCYOxu5vrAztGGxsZ" +
-    //     "hV0gvSeNKQ9qCw==" +
-    //     "-----END PRIVATE KEY-----", 'pkcs8-private-pem');
-    // const userPrivateKey = new NodeRSA("-----BEGIN PRIVATE KEY-----\n" +
-    //     "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBANZVkrUS0NpAQskZ\n" +
-    //     "S1qK5F9oh9BTsBuDoJZ3YWaz3h6U6R2kXq/rhUOG2qwPmHdNmpXQwMkyTfcwqZy4\n" +
-    //     "VmitE2RJVVqQ+jFKdviLOBJdK4VKFikUH7VpOJPu0JNmeGhfDhF3KItIQvMqaceb\n" +
-    //     "9CzxEcriEripRjlaEUwLGWBzliyVAgMBAAECgYAom+QE/MCnaV99/JXjXMdyMDrz\n" +
-    //     "RLfsLp9BgvLoY5HlF2+nlxn9mNER/z31Nas8h26PjhLIzAgrmoO4VtDAWnJ+x9dW\n" +
-    //     "0jK0aSDVQVOTkXwyj6ABVxS5h9Q96jxUS+Y90Q6wlrkcNjmbn2p1bpYZsZiAJlOx\n" +
-    //     "z0H0HZkxYKWloeq7oQJBAPnh5vwiiRW3m8TDl2ZTUB7/wFG+btNoi9Y1WmHjCvuM\n" +
-    //     "C219wZSQPtoF98pF93U533NGyxUC6hNQxh5N3AzAmx0CQQDblODhkVH9WEpm+0vf\n" +
-    //     "vOOsYpGZfs6f62qILhDfQXsCTzXRuBZOsxll7tBlOYLynQW+Vvx3LWgmS3C2p445\n" +
-    //     "baXZAkEAvqCDYh3Gjna/sl+0BC/Sk5Nv1+x7B3ReGVAiX0KhgjzYCvNA1mEkUZbz\n" +
-    //     "7a9qlFdOC9cZ3jGW+J1KRpwHHwNgUQJAeOfzI4EW+j7BSQ8Z6iph2quzrCcsstW3\n" +
-    //     "pSBoXeX6S30+Dyoj7lWjqILk8LfIKLrZTMMs5Jp1/JARkiGUqH59kQJAIJYzaJl7\n" +
-    //     "LZQhThS3/fiPEk6CvLSQMgsl++sxy/AiaRnv5vd5Pb6pZMOkBSNz59O7srHqXFmv\n" +
-    //     "cAtBnLujR54kqA==\n" +
-    //     "-----END PRIVATE KEY-----", 'pkcs8-private-pem');
-    //console.log(privateKey);
-    //privateKey = privateKey.replace(" ", "");
 
     console.log(privateKey);
 
     const userPrivateKey = new NodeRSA(privateKey, 'pkcs8-private-pem');
-
-    // const toUserPublicKey = new NodeRSA("-----BEGIN PUBLIC KEY-----" +
-    //     "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC4b8zJKqz3B4vZ2JSDpynSoctF" +
-    //     "ntZlEFwuG5I64Xo/fwRryPK+MK7VkTsN6DtWueBp1nZakMc0DclzmIDKSZR9BMNU" +
-    //     "e2ZNdbIDbRIHpNAzmcoIHc/pVbFbDOUGHv9mAsWfy6AjSHgowPpN1hg9Px0Jz2Dh" +
-    //     "3DDjpHHxP4NyWaJM3QIDAQAB" +
-    //     "-----END PUBLIC KEY-----", 'pkcs8-public-pem');
 
     let messageInParts = message.split("!part!");
     console.log("messageInParts", messageInParts);
--- src/pages/ServerPage.js
+++ src/pages/ServerPage.js
@@ -1,40 +1,61 @@
 import React, {useContext, useState} from "react";
 import {Link, useParams} from "react-router-dom";
 import {jwtContext} from "../App";
-import {List, ListItem, Typography} from "@material-ui/core";
+import {Container, Grid, List, ListItem, Paper, Typography} from "@material-ui/core";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListSubheader from '@material-ui/core/ListSubheader';
-import { FixedSizeList } from 'react-window';
+import {FixedSizeList} from 'react-window';
+import {makeStyles} from "@material-ui/core/styles";
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 let usersList = ['Wczytywanie'];
+let selectedAlias = "";
+
+const useStyles = makeStyles((theme) => ({
+    listStyle: {
+        maxHeight: 400,
+    },
+}));
 
 
-async function getUsers(jwt, id) {
-
-
-    const credentials = "Bearer " + jwt[0];
-
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", credentials);
-
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
-    const response = await fetch("http://localhost:8500/guild?guildName=" + id + "&content=members", requestOptions)
-    return await response.text();
-}
-
-function ServerPage(){
-    let { id } = useParams();
+function ServerPage() {
+    let {id} = useParams();
     const jsonWebToken = useContext(jwtContext);
     const [usersList, setUsersList] = React.useState([]);
+    const [dynamicCategoriesList, setDynamicCategoriesList] = React.useState([]);
+    const [aliasesList, setAliasesList] = React.useState([]);
+    const [rolesAllowedToUseList, setRolesAllowedToUseList] = React.useState([]);
 
+    const [aliasDialogOpen, setAliasDialogOpen] = React.useState(false);
+
+    const handleAliasDialogClose = () => {
+        setAliasDialogOpen(false);
+        setAliasToDelete("");
+    };
+
+    const classes = useStyles();
     const credentials = "Bearer " + jsonWebToken[0];
+
+
+    const [aliasToDelete, setAliasToDelete] = React.useState("");
+    const handleAliasDialogOpen = (aliasName) => {
+        setAliasToDelete(aliasName);
+        console.log("Alias do usuniecia:" + aliasName);
+        setAliasDialogOpen(true);
+    }
+
+    const handleAliasDelete = (aliasName) => {
+        console.log("Usuwam alias:" + aliasName);
+        handleAliasDialogClose();
+    }
 
     var myHeaders = new Headers();
     myHeaders.append("Authorization", credentials);
@@ -47,7 +68,7 @@ function ServerPage(){
 
 
     React.useEffect(function effectFunction() {
-        fetch("http://localhost:8500/guild?guildName=" + id + "&content=members", requestOptions)
+        fetch("http://localhost:8500/dashboardData?guildName=" + id, requestOptions)
             .then(function (response) {
                 if (!response.ok) {
                     throw new Error("HTTP status " + response.status);
@@ -56,7 +77,10 @@ function ServerPage(){
             })
             .then(function (result) {
                 console.log(result);
-                setUsersList(result.toString().substr(0, result.toString().length-1).split(","));
+                setUsersList(result.guildUserNames);
+                setDynamicCategoriesList(result.dynamicCategories);
+                setAliasesList(result.aliases);
+                setRolesAllowedToUseList(result.rolesAllowedToUse)
             })
             .catch(error => console.log('error', error));
 
@@ -65,24 +89,109 @@ function ServerPage(){
 
     return (
         <div>
-            <Typography variant={"h3"}>
-                {id}
-            </Typography>
-            <List
-                disablePadding
-            subheader={
-                <ListSubheader>
-                    Lista użytkowników
-                </ListSubheader>
-            }>
-                {usersList.map((text, index) => (
-                    <ListItem  button key={text}>
-                        <ListItemIcon><KeyboardArrowRightIcon/></ListItemIcon>
-                        <ListItemText primary={text}/>
-                    </ListItem>
-                ))}
-            </List>
+            <Container maxWidth={"xl"}>
+                <Typography variant={"h3"}>
+                    {id}
+                </Typography>
+                <Paper>
+                    <Grid container>
+
+                        <Grid item xs={3}>
+
+                            <List
+                                disablePadding
+                                subheader={
+                                    <ListSubheader>
+                                        Lista użytkowników
+                                    </ListSubheader>
+                                }>
+                                {usersList.map((user) => (
+                                    <ListItem button key={user}>
+                                        <ListItemIcon><KeyboardArrowRightIcon/></ListItemIcon>
+                                        <ListItemText primary={user}/>
+                                    </ListItem>
+                                ))}
+                            </List>
+
+                        </Grid>
+                        <Grid item xs={3}>
+
+                            <List
+                                disablePadding
+                                subheader={
+                                    <ListSubheader>
+                                        Lista dynamicznych kategorii
+                                    </ListSubheader>
+                                }>
+                                {dynamicCategoriesList.map((category, index) => (
+                                    <ListItem button key={category.category_name}>
+                                        <ListItemIcon><KeyboardArrowRightIcon/></ListItemIcon>
+                                        <ListItemText primary={category.category_name}/>
+                                    </ListItem>
+                                ))}
+                            </List>
+
+                        </Grid>
+                        <Grid item xs={3}>
+
+                            <List
+                                disablePadding
+                                subheader={
+                                    <ListSubheader>
+                                        Lista aliasów
+                                    </ListSubheader>
+                                }>
+                                {aliasesList.map((alias, index) => (
+                                    <ListItem button
+                                              key={alias.alias_name}
+                                              onClick={() => handleAliasDialogOpen(alias.alias_name)}
+                                    >
+                                        <ListItemIcon><KeyboardArrowRightIcon/></ListItemIcon>
+                                        <ListItemText primary={alias.alias_name}/>
+                                    </ListItem>
+                                ))}
+                            </List>
+
+                        </Grid>
+                        <Grid item xs={3}>
+                            <List
+                                disablePadding
+                                subheader={
+                                    <ListSubheader>
+                                        Lista dozwolonych roli
+                                    </ListSubheader>
+                                }>
+                                {rolesAllowedToUseList.map((role, index) => (
+                                    <ListItem button key={role.role_name}>
+                                        <ListItemIcon><KeyboardArrowRightIcon/></ListItemIcon>
+                                        <ListItemText primary={role.role_name}/>
+                                    </ListItem>
+                                ))}
+                            </List>
+
+                        </Grid>
+
+                    </Grid>
+                </Paper>
+            </Container>
+            <Dialog open={aliasDialogOpen} onClose={handleAliasDialogClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Czy na pewno chcesz usunąć {aliasToDelete}?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleAliasDialogClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={() => handleAliasDelete(aliasToDelete)} color="primary">
+                        Subscribe
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
+
     );
 }
 
